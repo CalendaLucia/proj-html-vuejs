d2ff6cb6dfacbe581bdd40ded1276af84c04a458
CalendaLucia <calendalucia4@gmail.com>
2023-02-13 12:57:28 +0100

    creato scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name:'App',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+ <AppHeader/>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name:'AppHeader',
+    data() {
+        return {
+            
+        }
+    },
+}
+
+</script>
+
+<template>
+ <h1>Scaffolding creato </h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color:rebeccapurple;
+}
+
+</style>
